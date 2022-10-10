4908920a449f600f89c6957a68be886a800a1eb3
DeadlyPants11 <100023619+DeadlyPants11@users.noreply.github.com>
2022-10-10 10:32:23 +0000

    Deploying to gh-pages from @ DeadlyPants11/project-10@3626bd76d2242b4ed65e47b633a52674f8a669aa 🚀

--- index.b464de50.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[modal-ingredients-open]"),closeModalBtn:document.querySelector("[modal-ingredients-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.b464de50.js.map
