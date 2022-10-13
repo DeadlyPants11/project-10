005ac4b82ab140ed5ae0dcc757ac950a56b77b93
DeadlyPants11 <100023619+DeadlyPants11@users.noreply.github.com>
2022-10-13 18:51:35 +0000

    Deploying to gh-pages from @ DeadlyPants11/project-10@d1ca438e24fca4c1596e74da76195956a075c1a8 🚀

--- index.a893c17d.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[modal-french-open]"),closeModalBtn:document.querySelector("[modal-french-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.a893c17d.js.map
